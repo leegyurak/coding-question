b9404e0e4c60e86cdf127d7be5fe9742b874c4bb
이규락 <gyurak.lee@qualson.com>
2022-07-05 23:35:29 +0900

    study: day1

--- 프로그래머스/1단계/3진법 뒤집기.py
+++ 프로그래머스/1단계/3진법 뒤집기.py
@@ -1,6 +1,6 @@
 three_notation_elements = []
 
-def three_notation(n):
+def three_notation(n): #1
     three_notation_elements.append(str(n%3))
     n = int(n/3)
 
@@ -12,8 +12,8 @@ def three_notation(n):
 def solution(n):
     three_notation(n)
 
-    three_notation_str = ''.join(three_notation_elements)
-    three_notation_str = str(int(three_notation_str))
+    three_notation_str = ''.join(three_notation_elements) #2
+    three_notation_str = str(int(three_notation_str)) #3
 
-    answer = int(three_notation_str, 3)
+    answer = int(three_notation_str, 3) #4
     return answer
