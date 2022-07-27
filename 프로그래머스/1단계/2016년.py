0c7ab05bfbc96832e42b23793821ebc04c0471a5
이규락 <gyurak.lee@qualson.com>
2022-07-27 23:56:24 +0900

    study: day5

--- 프로그래머스/1단계/2016년.py
+++ 프로그래머스/1단계/2016년.py
@@ -1,7 +1,9 @@
 def solution(a, b):
-    day_of_the_week_list = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'] #1
+    day = 0
+    day_of_the_week_list = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    day_of_the_week_idx = 0
 
-    month_day_list = [ #2
+    month_day_list = [
         [1, 31],
         [2, 29],
         [3, 31],
@@ -16,22 +18,13 @@ def solution(a, b):
         [12, 31]
     ]
 
-    day = 0
-
-    for month_day in month_day_list: #3
+    for month_day in month_day_list:
         if a == month_day[0]:
             day += b
             break
         else:
             day += month_day[1]
 
-    day_of_the_week_idx = 0
-
-    while True: #4
-        day %= 7
-
-        if day < 7:
-            day_of_the_week_idx = day - 1
-            break
+    day_of_the_week_idx = day % 7 - 1
             
     return day_of_the_week_list[day_of_the_week_idx]
